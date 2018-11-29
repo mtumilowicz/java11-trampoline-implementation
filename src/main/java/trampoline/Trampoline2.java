103420045a8cf7e62faf6e847097f5d63c7c2060
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-29 16:57:42 +0100

    trampoline2 fix + tests

--- src/main/java/trampoline/Trampoline2.java
+++ src/main/java/trampoline/Trampoline2.java
@@ -1,59 +1,62 @@
 package trampoline;
 
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 /**
  * Created by mtumilowicz on 2018-11-25.
  */
+@FunctionalInterface
 public interface Trampoline2<T> extends Supplier<T> {
 
-//    default Trampoline2<T> bounce() {
-//        return this;
-//    }
-//
-//    default T result() {
-//        return get();
-//    }
-//
-//    default boolean completed() {
-//        return true;
-//    }
-//
-//    public static <T> Trampoline2<T> done(final T result) {
-//        return () -> result;
-//    }
-//
-//    @Override
-//    T get();
-//
-//    public static <T> Trampoline2<T> more(final Trampoline2<Trampoline2<T>> trampoline) {
-//        return new Trampoline2<>() {
-//
-//
-//            @Override
-//            public boolean completed() {
-//                return false;
-//            }
-//
-//            @Override
-//            public Trampoline2<T> bounce() {
-//                return trampoline.result();
-//            }
-//
-//            @Override
-//            public T get() {
-//                return trampoline(this);
-//            }
-//
-//            T trampoline(final Trampoline2<T> trampoline) {
-//
-//                return Stream.iterate(trampoline, Trampoline2::bounce)
-//                        .filter(Trampoline2::completed)
-//                        .findFirst()
-//                        .get()
-//                        .result();
-//
-//            }
-//        };
-//    }
+    default Trampoline2<T> bounce() {
+        return this;
+    }
+    
+    default T result() {
+        return get();
+    }
+    
+    @Override
+    T get();
+    
+    default boolean complete() {
+        return true;
+    }
+
+    public static <T> Trampoline2<T> done(final T result) {
+        return () -> result;
+    }
+    
+    public static <T> Trampoline2<T> more(final Trampoline2<Trampoline2<T>> trampoline) {
+        return new Trampoline2<T>() {
+
+
+            @Override
+            public boolean complete() {
+                return false;
+            }
+
+            @Override
+            public Trampoline2<T> bounce() {
+                return trampoline.result();
+            }
+
+            @Override
+            public T get() {
+                return trampoline(this);
+            }
+
+            T trampoline(final Trampoline2<T> trampoline) {
+
+                return Stream.iterate(trampoline, Trampoline2::bounce)
+                        .filter(Trampoline2::complete)
+                        .findFirst()
+                        .orElseThrow()
+                        .result();
+
+            }
+        };
+    }
+
 }
--- src/test/java/FactTest.java
+++ src/test/java/FactTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import trampoline.Trampoline2;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -12,15 +13,26 @@ public class FactTest {
     public void xxx() {
         Fact fact = new Fact();
 
-        Long apply = fact.apply(5);
-        
+        Long apply = fact.apply(50);
+
         assertThat(apply, is(120L));
     }
 
     @Test
     public void xxx2() {
-//        Long apply = Fact2.fact(1_000_000, 1);
-//
-//        assertThat(apply, is(1307674368000L));
+        Long apply = Fact2.fact(1_000_000, 1);
+
+        assertThat(apply, is(1307674368000L));
+    }
+
+    @Test
+    public void xxx3() {
+        Integer result = chuj(1_000_000, 1).result();
+
+        assertThat(result, is(1307674368000L));
+    }
+
+    public Trampoline2<Integer> chuj(int n, int acc) {
+        return n == 1 ? Trampoline2.done(acc) : Trampoline2.more(() -> chuj(n - 1, n * acc));
     }
 }
