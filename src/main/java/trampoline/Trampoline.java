23ef992daee3ac4044a12576927013a85e0687aa
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-29 17:57:07 +0100

    removing intermediaryCall

--- src/main/java/trampoline/Trampoline.java
+++ src/main/java/trampoline/Trampoline.java
@@ -31,10 +31,6 @@ public interface Trampoline<T> {
         return new TerminalCall<>(result);
     }
 
-    static <T> Trampoline<T> more(Trampoline<T> intermediary) {
-        return new IntermediaryCall<>(intermediary);
-    }
-
     class TerminalCall<T> implements Trampoline<T> {
 
         private final T result;
@@ -57,31 +53,6 @@ public interface Trampoline<T> {
         public Trampoline<T> bounce() {
             // bouncing is over
             throw new UnsupportedOperationException();
-        }
-    }
-
-    class IntermediaryCall<T> implements Trampoline<T> {
-
-        private final Trampoline<T> previous;
-
-        public IntermediaryCall(Trampoline<T> previous) {
-            this.previous = previous;
-        }
-
-        @Override
-        public boolean isComplete() {
-            return false;
-        }
-
-        @Override
-        public T result() {
-            // result is not available yet
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Trampoline<T> bounce() {
-            return previous.bounce();
         }
     }
 }
